9bcabbdd8081dd6c07cf6d0bd9a213a28e806e6d
Christoph Rueff <christoph.rueff@gmail.com>
2019-11-09 14:18:47 +0100

    Now uses client session username to order

--- src/main/java/ch/heigvd/amt/presentation/OrderTestServlet.java
+++ src/main/java/ch/heigvd/amt/presentation/OrderTestServlet.java
@@ -1,6 +1,7 @@
 package ch.heigvd.amt.presentation;
 
 import ch.heigvd.amt.integration.IOrderDAO;
+import ch.heigvd.amt.model.Client;
 import ch.heigvd.amt.model.Order;
 import ch.heigvd.amt.model.OrderLine;
 
@@ -25,7 +26,7 @@ public class OrderTestServlet extends HttpServlet {
         List<OrderLine> orders = new ArrayList<>();
         orders.add(new OrderLine(3, 1));
         orders.add(new OrderLine(4, 2));
-        Order order = new Order(-1, "Aaron Alexander", null, orders);
+        Order order = new Order(-1, ((Client) req.getSession(false).getAttribute("client-session")).getUsername(), null, orders);
         try {
             orderDAO.create(order);
             resp.getWriter().println("create order");
